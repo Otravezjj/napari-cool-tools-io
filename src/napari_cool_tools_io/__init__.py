b7892cfc4877d3a8dd386fe4d24b7e144aa076b3
John Jackson <jdjacks@post.harvard.edu>
2023-05-23 13:23:45 -0700

    swap init with io

--- src/napari_cool_tools_io/__init__.py
+++ src/napari_cool_tools_io/__init__.py
@@ -6,3 +6,9 @@ except ImportError:
 from ._sample_data import make_sample_data
 
 __all__ = ("make_sample_data",)
+
+import napari
+import torch
+
+viewer = napari.current_viewer()
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
